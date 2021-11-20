0c56c05d71ae93135bda55a20fae1c7aed97ce6f
maxclingroth <maxclingroth@gmail.com>
2021-11-19 23:26:44 -0500

    Update doodle.py

--- doodle.py
+++ doodle.py
@@ -1,24 +1,4 @@
-import tkinter as tk
 
-
-class Paint:
-    def __init__(self):
-        self.master = tk.Tk()
-        self.master.geometry('750x750')
-        self.w = tk.Canvas(self.master, width=500, height=750)
-        self.w.pack(side='left')
-        self.menu = tk.Frame(self.master, width=250, height=750)
-        self.colorOptions = tk.Frame(self.menu)
-        self.colorVar = tk.IntVar(value=1)
-        self.black = tk.Radiobutton(self.colorOptions, text='Black', variable=self.colorVar, value=1)
-        self.red = tk.Radiobutton(self.colorOptions, text='Red', variable=self.colorVar, value=2)
-        self.blue = tk.Radiobutton(self.colorOptions, text='Blue', variable=self.colorVar, value=3)
-        self.green = tk.Radiobutton(self.colorOptions, text='Green', variable=self.colorVar, value=4)
-        self.eraser = tk.Radiobutton(self.colorOptions, text='Eraser', variable=self.colorVar, value=0)
-        self.black.pack()
-        self.red.pack()
-        self.blue.pack()
-        self.green.pack()
         self.eraser.pack()
         self.colorOptions.pack()
         self.menu.pack(side='right')
